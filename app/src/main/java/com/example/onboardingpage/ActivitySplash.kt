cf1e0164b95c605fd02747cc0f2b0ba618483ac4
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-03-29 17:10:47 +0700

    feat: first upload and add splash screen

---
+++ app/src/main/java/com/example/onboardingpage/ActivitySplash.kt
@@ -0,0 +1,23 @@
+package com.example.onboardingpage
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+
+class ActivitySplash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        // Full screen
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@ActivitySplash, OnboardingActivity::class.java))
+            finish()
+        }, 2000)
+
+    }
+}
